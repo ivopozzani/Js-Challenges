ee4c7f6f0016afe0564d9da95f1465b9db40c438
ivopozzani <ivo.pozzani@gmail.com>
2021-09-10 18:04:04 -0300

    update

--- Animating a Dog with Css Variables/main.js
+++ Animating a Dog with Css Variables/main.js
@@ -2,29 +2,58 @@ const dog = document.querySelector('.dog');
 
 let x = 0;
 let y = 0;
-const speed = 10;
+let rotate = 0;
+let rotateY = 0;
+let speed = 10;
 
-function handleKey (event) {
+function handleKeyDown (event) {
+  const number = Number(event.key); 
+
    if (event.key.includes('Arrow')) {
      switch (event.key) {
-      case 'ArrowUp' : y = y - 1;
+      case 'ArrowUp' : y = y - 1, rotate = -90, rotateY = 0;
       break;
-      case 'ArrowDown' : y = y + 1;
+      case 'ArrowDown' : y = y + 1, rotate = 90, rotateY = 0;
       break;
-      case 'ArrowLeft' : x = x - 1;
+      case 'ArrowLeft' : x = x - 1, rotate = 0, rotateY = -180;
       break;
-      case 'ArrowRight' : x = x + 1;
+      case 'ArrowRight' : x = x + 1, rotate = 0, rotateY = 0;
       break;
       default:
         break;
     }
-    dog.setAttribute('style',`--x: ${x * speed}px; --y: ${y * speed}px;`);   
-
-  } else {
-  console.log('Not apropriate key')}
+    dog.setAttribute('style',`--x: ${x * speed}px; --y: ${y * speed}px; --rotate: ${rotate}deg; --rotateY: ${rotateY}deg;`);
+  } 
+     
+    switch (number) {
+       case 0 : speed = 10;
+       break;
+       case 1 : speed = 20;
+       break;
+       case 2 : speed = 30;
+       break;
+       case 3 : speed = 40;
+       break;
+       case 4 : speed = 50;
+       break;
+       case 5 : speed = 60;
+       break;
+       case 6 : speed = 70;
+       break;
+       case 7 : speed = 80;
+       break;
+       case 8 : speed = 90;
+       break;
+       case 9 : speed = 100;
+       break;
+       default:
+       break;    
+      }  
 }
 
-window.addEventListener('keydown', handleKey)
+
+
+window.addEventListener('keydown', handleKeyDown)
 
 
 
