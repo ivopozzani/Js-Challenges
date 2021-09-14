7ad6ee576e34b6af33396aa7aae27ca9ee0d391b
ivopozzani <ivo.pozzani@gmail.com>
2021-09-14 16:56:13 -0300

    last update

--- Js Drum Kit/main.js
+++ Js Drum Kit/main.js
@@ -1,63 +1,18 @@
-const clap = document.querySelector('.clap audio')
-const hihat = document.querySelector('.hihat audio')
-const kick = document.querySelector('.kick audio')
-const openhat = document.querySelector('.openhat audio')
-const boom = document.querySelector('.boom audio')
-const ride = document.querySelector('.ride audio')
-const snare = document.querySelector('.snare audio')
-const tom = document.querySelector('.tom audio')
-const tink = document.querySelector('.tink audio')
 
-
-function handleKeyDown (event) {
-  switch (event.key) {
-    case 'a' : clap.play(), clap.closest('div').classList.add('large');
-     break;
-    case 's' : hihat.play(), hihat.closest('div').classList.add('large');
-     break;
-    case 'd' : kick.play(), kick.closest('div').classList.add('large');
-     break;
-    case 'f' : openhat.play(), openhat.closest('div').classList.add('large');
-     break;
-    case 'g' : boom.play(), boom.closest('div').classList.add('large');
-     break;
-    case 'h' : ride.play(), ride.closest('div').classList.add('large');
-      break;
-    case 'j' : snare.play(), snare.closest('div').classList.add('large');
-      break;
-    case 'k' : tom.play(), tom.closest('div').classList.add('large');
-      break;
-    case 'l' : tink.play(), tink.closest('div').classList.add('large');
-      break;
-    default:
-      break;
-  }
+function handleKeyDown(event){
+  const keyCode = document.querySelectorAll(`[data-key="${event.keyCode}"]`)
+  keyCode[0].classList.add('play')
+  keyCode[1].currentTime = 0;
+  keyCode[1].play()
 }
 
-function handleKeyUp (event) {
-  switch (event.key) {
-    case 'a' :  clap.closest('div').classList.remove('large');
-     break;
-    case 's' : hihat.closest('div').classList.remove('large');
-     break;
-    case 'd' :  kick.closest('div').classList.remove('large');
-     break;
-    case 'f' : openhat.closest('div').classList.remove('large');
-     break;
-    case 'g' :  boom.closest('div').classList.remove('large');
-     break;
-    case 'h' :  ride.closest('div').classList.remove('large');
-      break;
-    case 'j' : snare.closest('div').classList.remove('large');
-      break;
-    case 'k' : tom.closest('div').classList.remove('large');
-      break;
-    case 'l' :  tink.closest('div').classList.remove('large');
-      break;
-    default:
-      break;
-  }
+function handleTransition (event){
+  if (event.propertyName !== 'transform') return;
+  this.classList.remove('play')
 }
-document.addEventListener('keydown', handleKeyDown)
-document.addEventListener('keyup', handleKeyUp)
 
+const key = document.querySelectorAll('.key')
+
+key.forEach(key => key.addEventListener('transitionend', handleTransition))
+
+window.addEventListener('keydown',handleKeyDown)
