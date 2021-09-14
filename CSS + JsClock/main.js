b41a46dfbbe0f971a59511100af08a7129c8a4ee
ivopozzani <ivo.pozzani@gmail.com>
2021-09-14 17:59:06 -0300

    finished

--- CSS + JsClock/main.js
+++ CSS + JsClock/main.js
@@ -1,18 +1,17 @@
-const run = setInterval(runClock, 1000);
-
-function runClock(){
-const timeMs = Date.now();
-const sec = Math.floor(timeMs/1000);
-const min = Math.floor(sec/60);
-const hour = Math.floor(min/60);
-
 const secHand = document.querySelector('.sec');
 const minHand = document.querySelector('.min');
 const hourHand = document.querySelector('.hour');
 
- secHand.setAttribute('style', `transform: rotate(${sec*6}deg);`);
- minHand.setAttribute('style', `transform: rotate(${min*6}deg);`);
- hourHand.setAttribute('style', `transform: rotate(${hour*6}deg);`);
+function runClock(){
+  
+const timeMs = new Date();
+const sec = timeMs.getSeconds();
+const min = timeMs.getMinutes();
+const hour = timeMs.getHours();
+
+secHand.style.transform = `rotate(${(sec*6) + 90}deg)`
+minHand.style.transform = `rotate(${(min*6) + 90}deg)`
+hourHand.style.transform = `rotate(${(hour*30) + 90}deg)`
 }
 
-
+setInterval(runClock, 1000);
